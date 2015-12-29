e5422a10beb7ca233c7c673c8fb79fc630fd3c76
MarcinakaSin <marcinmu330@gmail.com>
2015-12-29 13:38:52 -0600

    adding routing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-var app = angular.module('mdbApp', []);
+var app = angular.module('mdbApp', [])
+	.config(function($routeProvider, $locationProvider){
+		$locationProvider.html5Mode(true);
+
+		$routeProvider.when("/", {templateUrl: "/sections/modify.html"});
+	});
